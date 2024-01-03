b4e125f81e3aac0d2aacdc61f326bb5dfc9e0d64
Natalie Rekedal Holbrook <nholbroo@c4a9c3.42berlin.de>
2024-01-03 16:56:02 +0100

    first commit

---
+++ get_next_line.c
@@ -0,0 +1,172 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nholbroo <nholbroo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/08 14:23:07 by nholbroo          #+#    #+#             */
+/*   Updated: 2024/01/03 16:41:01 by nholbroo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/*The first three functions are handling the current, next and last lines.
+The read_file-function is reading through the file descriptor, returning when
+it has encountered a newline somewhere in the last BUFFER_SIZE bytes read.
+The get_next_line-function is calling all functions, and keeping track
+of when it should stop reading.*/
+
+/*If there was no newline in the buffer, this last_line-function copies the 
+contents of the buffer into a new string, returning the whole string to 
+be output.*/
+
+static char	*last_line(char *readnow)
+{
+	char	*lastline;
+	int		i;
+
+	i = 0;
+	lastline = malloc(sizeof(char) * ft_strlen(readnow) + 1);
+	if (!lastline)
+		return (NULL);
+	while (readnow[i] != '\0')
+	{
+		lastline[i] = readnow[i];
+		i++;
+	}
+	lastline[i] = '\0';
+	return (lastline);
+}
+
+/*The next_line-function is returning a string containing the bytes that
+was read AFTER the current line, so I can store them to next time function
+is called. If there are no bytes, the next time the function is called,
+the function will return NULL.*/
+static char	*next_line(char *readnow)
+{
+	int		bfrsize;
+	int		start;
+	int		i;
+	char	*nextline;
+
+	if (ft_strchr_index(readnow, '\n') < 0)
+		return (NULL);
+	i = 0;
+	start = ft_strchr_index(readnow, '\n') + 1;
+	bfrsize = (int)ft_strlen(readnow) - ft_strchr_index(readnow, '\n');
+	nextline = malloc(sizeof(char) * bfrsize + 1);
+	if (!nextline)
+		return (NULL);
+	while (i < bfrsize)
+	{
+		nextline[i] = readnow[start];
+		i++;
+		start++;
+	}
+	nextline[i] = '\0';
+	return (nextline);
+}
+
+/*Returning a string containing the line I want to
+output, including the newline-character. Also handling the
+last line if there is no newline-character in the string passed to 
+the function.*/
+static char	*current_line(char *readnow)
+{
+	int		end;
+	int		i;
+	char	*currline;
+
+	i = 0;
+	if (ft_strchr_index(readnow, '\n') < 0)
+		return (last_line(readnow));
+	end = ft_strchr_index(readnow, '\n');
+	currline = malloc(sizeof(char) * end + 2);
+	if (!currline)
+		return (NULL);
+	while (i <= end)
+	{
+		currline[i] = readnow[i];
+		i++;
+	}
+	currline[i] = '\0';
+	return (currline);
+}
+
+/*Reading through the file until it 
+contains a newline, or if the file has ended*/
+static char	*read_file(char *remainder, int fd)
+{
+	char	*buffer;
+	int		bytesread;
+	char	*readnow;
+
+	readnow = remainder;
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytesread = 1;
+	while (bytesread > 0)
+	{
+		bytesread = read(fd, buffer, BUFFER_SIZE);
+		if (bytesread == -1)
+			return (ft_free(buffer));
+		buffer[bytesread] = '\0';
+		readnow = ft_strjoin(readnow, buffer);
+		if (!readnow)
+			return (NULL);
+		if (ft_strchr_index(readnow, '\n') > -1)
+			break ;
+	}
+	free(buffer);
+	return (readnow);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*remainder = NULL;
+	char		*output;
+	char		*readnow;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	readnow = read_file(remainder, fd);
+	if (!readnow)
+	{
+		free(remainder);
+		remainder = NULL;
+		return (NULL);
+	}
+	if (readnow[0] == '\0')
+	{
+		free(readnow);
+		readnow = NULL;
+		remainder = NULL;
+		return (NULL);
+	}
+	output = current_line(readnow);
+	remainder = next_line(readnow);
+	free(readnow);
+	readnow = NULL;
+	return (output);
+}
+
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*output;
+// 	int		i;
+
+// 	i = 0;
+// 	fd = open("test.txt", O_RDONLY);
+// 	output = get_next_line(fd);
+// 	while (output)
+// 	{
+// 		printf("%s", output);
+// 		free(output);
+// 		output = get_next_line(fd);
+// 	}
+// 	free(output);
+// }
---
+++ get_next_line_utils.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nholbroo <nholbroo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/08 14:23:12 by nholbroo          #+#    #+#             */
+/*   Updated: 2024/01/03 15:40:48 by nholbroo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_free(void *ptr)
+{
+	free(ptr);
+	ptr = NULL;
+	return (NULL);
+}
+
+static char	*ft_strjoin_cat_part(char *s1, char *s2, char *result)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		result[i] = s2[j];
+		j++;
+		i++;
+	}
+	result[i] = '\0';
+	ft_free(s1);
+	return (result);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*result;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!s1)
+	{
+		s1 = malloc(sizeof(char));
+		if (!s1)
+			return (ft_free(s2));
+		s1[0] = 0;
+	}
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	result = (char *)malloc(i + j + 1);
+	if (!result)
+	{
+		ft_free(s1);
+		return (ft_free(s2));
+	}
+	result = ft_strjoin_cat_part(s1, s2, result);
+	return (result);
+}
+
+int	ft_strchr_index(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s || c > 127)
+		return (-1);
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+	if (s[i] == '\0' && s[i] != c)
+		return (-1);
+	return (i);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
